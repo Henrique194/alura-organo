5bb6a8dbcf49655c06d7e98224a2f3db87d4e625
Henrique194 <henriquejb194@gmail.com>
2023-02-28 17:25:21 -0300

    refactor: lifting state up, removal of useEffect, removal of unnecessary class, reordering of files

--- src/common/Collaborator.js
+++
@@ -1,8 +0,0 @@
-export default class Collaborator {
-    constructor(course, imageUrl, name, role) {
-        this.course = course;
-        this.imageUrl = imageUrl;
-        this.name = name;
-        this.role = role;
-    }
-}
---
+++ src/components/Course/Course.js
@@ -0,0 +1,39 @@
+import "./Course.scss";
+import Card from "../Card";
+import { useState } from "react";
+import hexToRgba from "hex-to-rgba";
+
+export function Course({ course, onRemoveCollaborator }) {
+    const [colorState, setColorState] = useState(course.color);
+
+    if (course.collaborators.length === 0) return;
+
+    const styleSection = {
+        backgroundColor : hexToRgba(colorState, '0.6'),
+        backgroundImage: "url('images/fundo.png')"
+    };
+
+    const styleCourseName = { borderColor: colorState };
+
+    const cards = course.collaborators.map((collaborator, index) =>
+        <Card
+            key={collaborator.name}
+            backgroundColor={colorState}
+            collaborator={collaborator}
+            onRemoveCollaborator={onRemoveCollaborator(course, index)}
+        />
+    );
+
+    return (
+        <section className="course-component" style={ styleSection }>
+            <input
+                className="input-color"
+                onChange={event => setColorState(event.target.value)}
+                type="color"
+                value={colorState}
+            />
+            <h3 style={styleCourseName}>{course.name}</h3>
+            <div className="cards">{cards}</div>
+        </section>
+    );
+}
