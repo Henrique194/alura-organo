7b554f60d3c36024944d6e5e83d29aef9b8690df
Henrique194 <henriquejb194@gmail.com>
2023-03-01 08:54:13 -0300

    feat: favorite icon in collaborator card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import "./Card.scss";
-import { AiFillCloseCircle } from "react-icons/ai";
+import {AiFillCloseCircle, AiFillHeart, AiOutlineHeart} from "react-icons/ai";
+import {useState} from "react";
 
 export function Card({ backgroundColor, collaborator, onRemoveCollaborator }) {
     return (
@@ -15,7 +16,24 @@ export function Card({ backgroundColor, collaborator, onRemoveCollaborator }) {
             <div className="footer">
                 <h4>{collaborator.name}</h4>
                 <h5>{collaborator.role}</h5>
+                <FavoriteIcon />
             </div>
         </div>
+    );
+}
+
+function FavoriteIcon() {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    function handleClick() {
+        setIsFavorite(!isFavorite);
+    }
+
+    return (
+        <>
+            { isFavorite ? <AiFillHeart color="#FF0000" onClick={handleClick} size={25} />
+                         : <AiOutlineHeart onClick={handleClick} size={25} />
+            }
+        </>
     );
 }
